3e0ec4210a67d7f5689adca89063dd26620d0be2
jklof <janne.kaleva.lof@gmail.com>
2014-11-28 22:13:07 +0000

    Add history

--- client.js
+++ client.js
@@ -5,14 +5,38 @@ var SayText = angular.module('SayText', []);
 SayText.controller('SayController', function($scope){
 
     $scope.saytext = '';
+    $scope.sayhistory = [];
 
-    $scope.doSay = function() {
+    $scope.say = function(url) {
+        // play the sound
         var e = document.getElementById('say_snd');
         e.volume = 0.3;
-        e.src = "say?text=" + encodeURIComponent($scope.saytext);
+        e.src = url;
         e.load();
         e.play();
-        $scope.saytext = '';  
+    };
+
+    $scope.doSay = function() {
+        
+        if ($scope.saytext === '') {
+            return;
+        }
+        
+        // save url
+        var h = {
+            url : "say?text=" + encodeURIComponent($scope.saytext),
+            text : $scope.saytext
+        };
+
+        $scope.say(h.url);
+
+        // add to array
+        $scope.sayhistory.unshift(h);
+        // remove older elements
+        while ($scope.sayhistory.length > 10) {
+            $scope.sayhistory.pop();
+        }
+        $scope.saytext = '';
     };
 
     $scope.keyPress = function(event) {
